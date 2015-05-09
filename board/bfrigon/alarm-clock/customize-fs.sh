91292a20ddbffbb5d60ba3885d0541f322ac2ca2
Benoit Frigon <bfrigon@gmail.com>
2015-05-09 17:55:04 -0400

    Fix customize-fs: no error if directory already exist

--- board/bfrigon/alarm-clock/customize-fs.sh
+++ board/bfrigon/alarm-clock/customize-fs.sh
@@ -5,8 +5,8 @@ chmod +x output/target/etc/init.d/S92sensors
 
 
 # create mount points
-mkdir output/target/boot
-mkdir output/target/storage
+mkdir -p output/target/boot
+mkdir -p output/target/storage
 
 
 
